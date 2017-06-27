1a3e282c10d1351a70f8de9d8fdc8766f6f7a3d2
Michael Angerman <stormasm@gmail.com>
2017-06-27 16:11:30 -0700

    tests are up and running

--- build_alias.sh
+++ build_alias.sh
@@ -1,1 +1,7 @@
 alias ex1='./node_modules/.bin/babel-node examples/ex1.js'
+
+alias sw1='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require ./resources/mocha-bootload --check-leaks --full-trace ./test/http-test.js'
+
+alias sw2='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require ./resources/mocha-bootload --check-leaks --full-trace ./test/usage-test.js'
+
+alias swall='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require ./resources/mocha-bootload --check-leaks --full-trace ./test/*-test.js'
