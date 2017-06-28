c8176a32e3a1d0bbd0d6dd75cd1b40ba18d3b838
Michael Angerman <stormasm@gmail.com>
2017-06-27 17:01:56 -0700

    working on post-test

--- build_alias.sh
+++ build_alias.sh
@@ -4,4 +4,6 @@ alias sw1='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require .
 
 alias sw2='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require ./resources/mocha-bootload --check-leaks --full-trace ./test/usage-test.js'
 
+alias sw3='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require ./resources/mocha-bootload --check-leaks --full-trace ./test/post-test.js'
+
 alias swall='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require ./resources/mocha-bootload --check-leaks --full-trace ./test/*-test.js'
